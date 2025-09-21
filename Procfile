f3fb0eb691b5d248bcef305667e5fd078086afb9
KhoaCaper <khoacaper@gmail.com>
2025-09-21 16:27:05 +0700

    Initial commit: Spring Boot project setup for Railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/hokori-web-0.0.1-SNAPSHOT.jar
---
+++ src/test/java/com/hokori/web/HokoriWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hokori.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HokoriWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
